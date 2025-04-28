2e86d0faf68437082be60ce680a0158aa7282243
jkim188uncc <jkim188@charlotte.edu>
2025-04-28 14:01:39 -0400

    django base changes.
    started work on making accounts for people

--- MAKE_JID/base/models.py
+++ MAKE_JID/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# base/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Account(AbstractUser):
+    organization_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+class Event(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='events')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} - {self.account.organization_name}"
+
--- MAKE_JID/base/views.py
+++ MAKE_JID/base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# base/views.py
+
+from django.shortcuts import render, redirect
+from .forms import EventForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.account = request.user  # link event to the logged-in account
+            event.save()
+            return redirect('home')  # or wherever you want
+    else:
+        form = EventForm()
+
+    return render(request, 'base/create_event.html', {'form': form})
