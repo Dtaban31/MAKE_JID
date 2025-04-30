d2f7703dab45367432e04b3489dea4a0f331665a
jkim188uncc <jkim188@charlotte.edu>
2025-04-30 14:44:39 -0400

    Update
    Got signup and create event functions working

--- MAKE_JID/base/models.py
+++ MAKE_JID/base/models.py
@@ -1,11 +1,13 @@
+# base/models.py
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
+# Custom user model
 class Account(AbstractUser):
-    # override the default M2M fields to give them unique reverse names
+    # You can override fields or add new ones here
     groups = models.ManyToManyField(
         Group,
-        related_name='account_set',       # change this from default 'user_set'
+        related_name='account_set',  # Change from 'user_set'
         blank=True,
         help_text=AbstractUser._meta.get_field('groups').help_text,
         verbose_name=AbstractUser._meta.get_field('groups').verbose_name,
@@ -13,9 +15,21 @@ class Account(AbstractUser):
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='account_set',       # change this from default 'user_set'
+        related_name='account_set',  # Change from 'user_set'
         blank=True,
         help_text=AbstractUser._meta.get_field('user_permissions').help_text,
         verbose_name=AbstractUser._meta.get_field('user_permissions').verbose_name,
         related_query_name='account',
     )
+
+# Event model
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='events')  # Link event to Account
+
+    def __str__(self):
+        return self.title
