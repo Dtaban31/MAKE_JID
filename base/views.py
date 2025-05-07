f991cd519876874a1ab903bbc2caa6297a248d35
Dtaban31 <dctaban31@gmail.com>
2025-05-07 13:24:35 -0400

    Add files via upload

--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+rooms = [
+    {'id': 1, 'name':'Login'},
+    {'id': 2, 'name':'Post Creation'},
+]
+
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request,'base/home.html', context)
+
+def clubs(request, pk):
+
+    room = None
+    for i in rooms:
+        if i['id']== int(pk):
+            room = i
+    context = {'room': room}
+    return render(request,'base/clubs.html', context)
